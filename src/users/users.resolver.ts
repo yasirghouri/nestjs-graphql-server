25ebe3730874ff94b60e9a2095e07aed05547023
yasirghouri <yasir.ag7@gmail.com>
2021-03-05 18:42:25 +0500

    added inputTypes for users DTO and finished resolver and service code

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -4,6 +4,9 @@ import { User } from './models/user';
 import { UsersService } from './users.service';
 import { GetUserArgs } from './dto/args/get-user.args';
 import { GetUsersArgs } from './dto/args/get-users.args';
+import { CreateUserInput } from './dto/input/create-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
+import { DeleteUserInput } from './dto/input/delete-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -11,16 +14,26 @@ export class UsersResolver {
 
   @Query(() => User, { name: 'user', nullable: true })
   getUser(@Args() getUserArgs: GetUserArgs): User {
-    return this.usersService.getUser();
+    return this.usersService.getUser(getUserArgs);
   }
 
   @Query(() => [User], { name: 'users', nullable: 'items' })
   getUsers(@Args() getUsersArgs: GetUsersArgs): User[] {
-    return this.usersService.getUsers();
+    return this.usersService.getUsers(getUsersArgs);
   }
 
   @Mutation(() => User)
-  createUser(): User {
-    return this.usersService.createUser();
+  createUser(@Args('createUserData') createUserData: CreateUserInput): User {
+    return this.usersService.createUser(createUserData);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args('updateUserData') updateUserData: UpdateUserInput): User {
+    return this.usersService.updateUser(updateUserData);
+  }
+
+  @Mutation(() => User)
+  deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): User {
+    return this.usersService.deleteUser(deleteUserData);
   }
 }
